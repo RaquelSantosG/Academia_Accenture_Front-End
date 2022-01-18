b9d508d5e07edc9d7651387620db4962bbf2d18f
raquelnew <raquelnew3@gmail.com>
2022-01-17 22:23:24 -0300

    exercicio01

---
+++ exemplo01/src/App.js
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <>
+    <form>
+      <input type="text" placeholder="Raquel" />
+      <textarea placeholder="Fale sobre vc..." />
+    </form>
+
+    <ul>
+      <li>
+        <section>
+          <header>
+            <h2>Titulo</h2>
+          </header>
+          <p>Fale sobre vc</p>
+        </section>
+      </li>
+    </ul>
+    </>
+      );
+  }
+
+export default App;
